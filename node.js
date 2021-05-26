7add83b57124116185b17653cc83aa1e74993506
IsmaelAlomari <ismaelalomari994@gmail.com>
2021-05-27 00:11:09 +0300

    solved

--- node.js
+++ node.js
@@ -9,62 +9,51 @@ class Node {
 }
 
 
-const node7 = new Node(7,`I turned seven`)
-// console.log(`Node 1: ${age}: 1, ${highlight}: I was born`)
-
-const node3 = new Node(3,`I started walking`,node7)
-// console.log(`Node 3: ${age}: 3, ${highlight}: I started walking`)
-
-const node1 = new Node(1,`I was born`,node3)
-// console.log(`Node 3: ${age}: 7, ${highlight}: I turned seven`)
-
-
 class LinkedList {
-    constructor(age,highlight, nextNode = null) {
-      this.headNode = new Node(age,highlight);
+    constructor(age,highlight) {
+      this.head = new Node(age,highlight);
     }
+
+
  insertBegining = (age,highlight) => {
-     const node = new Node(age,highlight, this.head);
+     const node = new Node(age, highlight, this.head);
      this.head = node
      
- }
-
+    }
 getLinkedList = () => {
-    let current = this.head
+    let current = this.head;
     while(current) {
         console.log(`year : ${current.age} , highlight : ${current.highlight}`)
         current = current.nextNode;
-    }
-}
-
-insertHighLight = (age) => {
+     }
+   }
+ insertHighLight = (age) => {
     let current = this.head
     while(current.age < age){
         let currentAge = current.age + 1
         if (current.nextNode && current.nextNode.age === currentAge) {
-           current = currentAge.nextNode 
+           current = current.nextNode 
         }
         else {
             let highlight = prompt(`center highlight for year : ${currentAge} : `)
-            let newNode = new Node(currentAge,highlight,currentAge.nextNode)
+            let newNode = new Node(currentAge,highlight,current.nextNode)
             current.nextNode = newNode
             current = newNode;
         }
         
     }
 }
-
 }
 
-
+ 
 
 const ageList = new LinkedList(7,"I was seven")
 ageList.insertBegining(3,"I start walking")
 ageList.insertBegining(1,"I was born")
 
-let age = prompt("age : ")
-ageList.insertHighLight(age)
-ageList.getLinkedList()
+let age = prompt("age : ");
+ageList.insertHighLight(age);
+ageList.getLinkedList();
 
 
 
