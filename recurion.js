d7f2db43f55838ee2aa26ea1a4e20a684358842e
IsmaelAlomari <ismaelalomari994@gmail.com>
2021-06-01 17:21:58 +0300

    soved

---
+++ recurion.js
@@ -0,0 +1,18 @@
+const numbers = [
+    [1, 2, 3, 4],
+    [3, 6, [5, 6], 8, 2,[2, 4], 9],
+    [4, 2, [6, 7, [2, 6, 1]]],
+]
+
+
+const sum = (nums) => {
+    let total = 0;
+    nums.forEach((num) => {
+        if (!isNaN(num)) total += num
+        else total += sum(num);
+    });
+    return total
+}
+
+
+console.log(sum(numbers))
